15d70dd3fabf9b5e5d1211eda686eb582acd32f9
babita-chaube <57911612+babita-chaube@users.noreply.github.com>
2019-11-18 18:27:51 -0600

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,9 @@
+package src.main.java.rulesEngine
+public class Person{
+int credit_score;
+String state;
+public Person(int credit_score,String state){
+this.credit_score=credit_score;
+this.state=state;
+                                            }
+                   }
