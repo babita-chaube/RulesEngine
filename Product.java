93bd01060db517ff55db3b549859ee92621fde6d
babita-chaube <57911612+babita-chaube@users.noreply.github.com>
2019-11-18 18:32:27 -0600

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,10 @@
+package src.main.java.rulesEngine;
+public class Product{
+ String name;
+ public double interest_rate;
+ public boolean disqualified;
+ public Product(String name,double interest_rate){
+   this.name=name;
+   this.interest_rate=interest_rate;            
+                                                }
+                         }
